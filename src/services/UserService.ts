8df390c9cef35c376dd33df8cd880d262c6b1d77
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-07 18:22:21 -0300

    feat: update registerUser function to return response data instead of void

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,16 +1,7 @@
-import { isAxiosError } from 'axios'
 import type { RegisterUser } from '@/types/UserTypes'
 import axios from '@/axiosConfig'
 
-/** Registrar un usuario */
-export const registerUser = async (payload: RegisterUser): Promise<void> => {
-  try {
-    await axios.post('auth/register', payload)
-  } catch (error: unknown) {
-    if (isAxiosError(error) && error.response) {
-      throw new Error(error.response.data?.message || 'Error while registering user')
-    } else {
-      throw new Error('Error while registering user')
-    }
-  }
+export const registerUser = async (payload: RegisterUser): Promise<{ frontend_url: string }> => {
+  const response = await axios.post('/auth/register', payload)
+  return response.data
 }
