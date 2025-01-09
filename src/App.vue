ace9833e915e0cbcca731a5064bff157bd31d568
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-09 18:56:49 -0300

    fix: update navigation link from Inventory to Add Product for authenticated users

--- src/App.vue
+++ src/App.vue
@@ -74,8 +74,8 @@ onMounted(() => {
         class="mt-4 md:mt-0"
       >
         <!-- Enlaces condicionales -->
-        <RouterLink v-if="isAuthenticated" to="/inventory" class="block px-2 py-1">
-          Inventory
+        <RouterLink v-if="isAuthenticated" to="/add-product" class="block px-2 py-1">
+          Add Product
         </RouterLink>
         <RouterLink v-if="!isAuthenticated && isMainDomain" to="/register" class="block px-2 py-1">
           Register
