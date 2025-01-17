d75c7ad9e32164a830eb26d11fdc620d0fb1353d
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-17 19:07:28 -0300

    add RolePermissionTypes interface for roles and permissions management

---
+++ src/types/RolePermissionTypes.ts
@@ -0,0 +1,16 @@
+export interface Permission {
+  id: number;
+  name: string;
+  guard_name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Role {
+  id: number;
+  name: string;
+  guard_name: string;
+  created_at: string;
+  updated_at: string;
+  permissions: Permission[];
+}
