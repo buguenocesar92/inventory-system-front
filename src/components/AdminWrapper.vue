109b4a9837f028eb7ddd4a8d61fd2375efb904d3
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-28 19:31:03 -0300

    feat: add FooterAdmin component to AdminWrapper for enhanced layout

--- src/components/AdminWrapper.vue
+++ src/components/AdminWrapper.vue
@@ -6,6 +6,7 @@
       <div class="px-4 md:px-10 mx-auto w-full py-4">
         <slot />
       </div>
+      <footer-admin />
     </div>
   </div>
 </template>
@@ -13,8 +14,8 @@
 <script>
 import Sidebar from '@/components/Sidebar/Sidebar.vue';
 import AdminNavbar from '@/components/Navbars/AdminNavbar.vue';
-
+import FooterAdmin from "@/components/Footers/FooterAdmin.vue";
 export default {
-  components: { Sidebar, AdminNavbar },
+  components: { Sidebar, AdminNavbar, FooterAdmin },
 };
 </script>
