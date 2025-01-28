62ea85dcec69248de45f6af58544bd7b8c5b9d36
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-28 19:18:08 -0300

    feat: add AdminWrapper component for consistent layout across admin pages

---
+++ src/components/AdminWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Sidebar />
+    <div class="relative md:ml-64 bg-blueGray-100 min-h-screen">
+      <AdminNavbar />
+      <div class="px-4 md:px-10 mx-auto w-full py-4">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar/Sidebar.vue';
+import AdminNavbar from '@/components/Navbars/AdminNavbar.vue';
+
+export default {
+  components: { Sidebar, AdminNavbar },
+};
+</script>
