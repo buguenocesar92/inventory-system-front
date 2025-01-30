46e2d4df36995f1a2a1c7f4fb14801e6d24ed9b5
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-30 14:27:36 -0300

    feat: update icons in sidebar and logout button to use Vuetify components

--- src/components/LogoutButton.vue
+++ src/components/LogoutButton.vue
@@ -4,16 +4,8 @@
     href="#"
     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
   >
-    <svg
-      class="w-5 h-5 text-red-500"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-    </svg>
-    <span class="ms-3 text-red-500">Cerrar sesión</span>
+    <v-icon class="text-red-500">mdi-logout</v-icon>
+    <span class="ml-3 text-red-500">Cerrar sesión</span>
   </a>
 </template>
 
--- src/components/SidebarItem.vue
+++ src/components/SidebarItem.vue
@@ -1,24 +1,11 @@
 <!-- SidebarItem.vue -->
 <template>
-<RouterLink
-  :to="item.route"
-  class="flex items-center p-2 rounded-lg text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-  active-class="bg-gray-200 dark:bg-gray-700"
->
-    <svg
-      class="w-5 h-5"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        :d="item.icon"
-      />
-    </svg>
+  <RouterLink
+    :to="item.route"
+    class="flex items-center p-2 rounded-lg text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+    active-class="bg-gray-200 dark:bg-gray-700"
+  >
+    <v-icon class="w-5 h-5">{{ item.icon }}</v-icon>
     <span class="ml-3">{{ item.label }}</span>
   </RouterLink>
 </template>
@@ -29,10 +16,11 @@ import { defineProps } from 'vue';
 interface SidebarItem {
   label: string;
   route: string;
-  icon: string;
+  icon: string; // Ahora los íconos son nombres de Vuetify
 }
 
 defineProps<{
   item: SidebarItem
 }>();
 </script>
+
