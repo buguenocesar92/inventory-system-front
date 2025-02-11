a5393990e4cb100c7a6fd59a2c2ff57006dcf11a
buguenocesar92 <buguenocesar92@gmail.com>
2025-02-11 19:29:41 -0300

    feat: remove location and warehouse selection from AdminNavbar component

--- src/components/Navbars/AdminNavbar.vue
+++ src/components/Navbars/AdminNavbar.vue
@@ -6,54 +6,12 @@
         Dashboard
       </a>
 
-      <!-- Contenedor de Selects y User -->
-      <div class="flex items-center space-x-4">
-        <!-- 🔹 Select de Locales -->
-        <div class="w-52">
-          <FormSelect
-            v-model="selectedLocation"
-            id="location_id"
-            label="Seleccionar Local"
-            :options="locations"
-            placeholder="Seleccione un local"
-            placeholderValue="0"
-            required
-            @change="handleLocationChange"
-          />
-        </div>
-
-        <!-- 🔹 Select de Bodegas -->
-        <div class="w-72">
-          <FormSelect
-            v-model="selectedWarehouse"
-            id="warehouse_id"
-            label="Seleccionar Bodega"
-            :options="locationWarehouseStore.warehouseList"
-            placeholder="Seleccione una bodega"
-            placeholderValue="0"
-            required
-            @change="handleWarehouseChange"
-          />
-        </div>
-
         <!-- Dropdown de usuario, etc. -->
         <user-dropdown />
       </div>
-    </div>
   </nav>
 </template>
 
 <script setup lang="ts">
-import { useLocationWarehouseSelect } from "@/composables/useLocationWarehouseSelect";
-import FormSelect from "@/components/FormSelect.vue";
 import UserDropdown from "@/components/Dropdowns/UserDropdown.vue";
-
-const {
-  locationWarehouseStore,
-  locations,
-  selectedLocation,
-  selectedWarehouse,
-  handleLocationChange,
-  handleWarehouseChange,
-} = useLocationWarehouseSelect();
 </script>
